804386fa728d367fe3ea517b211ccfb09cd8647b
zhaozihao <18521706275@163.com>
2020-08-22 17:15:30 +0800

    commit

---
+++ addLib/src/main/java/com/domain/android/addlib/AddTest.java
@@ -0,0 +1,12 @@
+package com.domain.android.addlib;
+
+import android.util.Log;
+
+public class AddTest {
+
+    public static int add(int x,int y){
+        Log.e("zz",x+y+"");
+
+        return x+y;
+    }
+}
